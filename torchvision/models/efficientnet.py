706548b33aba12a40c3eb26d6843e479cbc1bf31
なるみ <weaper@gmail.com>
2019-06-03 11:31:46 +0800

    Add Efficient Nets

---
+++ torchvision/models/efficientnet.py
@@ -0,0 +1,240 @@
+import math
+
+import torch
+from torch import nn
+
+from .utils import load_state_dict_from_url
+
+model_urls = {
+    'efficientnet-b0': None,
+    'efficientnet-b1': None,
+    'efficientnet-b2': None,
+    'efficientnet-b3': None,
+    'efficientnet-b4': None,
+    'efficientnet-b5': None,
+    'efficientnet-b6': None,
+    'efficientnet-b7': None,
+}
+
+
+class Swish(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(Swish, self).__init__()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(
+                in_planes,
+                out_planes,
+                kernel_size,
+                stride,
+                padding,
+                groups=groups,
+                bias=False),
+            nn.BatchNorm2d(out_planes, eps=1e-3, momentum=0.01),
+            Swish(),
+        )
+
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self, in_planes, reduced_dim):
+        super(SqueezeExcitation, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_planes, reduced_dim, 1),
+            Swish(),
+            nn.Conv2d(reduced_dim, in_planes, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.se(x)
+
+
+class MBConvBlock(nn.Module):
+    def __init__(self,
+                 in_planes,
+                 out_planes,
+                 expand_ratio,
+                 kernel_size,
+                 stride,
+                 reduction_ratio=4,
+                 drop_connect_rate=0.2):
+        super(MBConvBlock, self).__init__()
+        self.drop_connect_rate = drop_connect_rate
+        self.use_residual = in_planes == out_planes and stride == 1
+        assert stride in [1, 2]
+        assert kernel_size in [3, 5]
+
+        hidden_dim = in_planes * expand_ratio
+        reduced_dim = max(1, int(in_planes / reduction_ratio))
+
+        layers = []
+        # pw
+        if in_planes != hidden_dim:
+            layers += [ConvBNReLU(in_planes, hidden_dim, 1)]
+
+        layers += [
+            # dw
+            ConvBNReLU(
+                hidden_dim,
+                hidden_dim,
+                kernel_size,
+                stride=stride,
+                groups=hidden_dim),
+            # se
+            SqueezeExcitation(hidden_dim, reduced_dim),
+            # pw-linear
+            nn.Conv2d(hidden_dim, out_planes, 1, bias=False),
+            nn.BatchNorm2d(out_planes, eps=1e-3, momentum=0.01),
+        ]
+
+        self.conv = nn.Sequential(*layers)
+
+    def _drop_connect(self, x):
+        if not self.training:
+            return x
+        keep_prob = 1.0 - self.drop_connect_rate
+        batch_size = x.size(0)
+        random_tensor = keep_prob
+        random_tensor += torch.rand(batch_size, 1, 1, 1, device=x.device)
+        binary_tensor = random_tensor.floor()
+        return x.div(keep_prob) * binary_tensor
+
+    def forward(self, x):
+        if self.use_residual:
+            return x + self._drop_connect(self.conv(x))
+        else:
+            return self.conv(x)
+
+
+def _make_divisible(value, divisor=8):
+    new_value = max(divisor, int(value + divisor / 2) // divisor * divisor)
+    if new_value < 0.9 * value:
+        new_value += divisor
+    return new_value
+
+
+def _round_filters(filters, width_mult):
+    if width_mult == 1.0:
+        return filters
+    return int(_make_divisible(filters * width_mult))
+
+
+def _round_repeats(repeats, depth_mult):
+    if depth_mult == 1.0:
+        return repeats
+    return int(math.ceil(depth_mult * repeats))
+
+
+class EfficientNet(nn.Module):
+    def __init__(self,
+                 width_mult=1.0,
+                 depth_mult=1.0,
+                 dropout_rate=0.2,
+                 num_classes=1000):
+        super(EfficientNet, self).__init__()
+
+        settings = [
+            # t, c, n, s, k
+            [1, 16, 1, 1, 3],
+            [6, 24, 2, 2, 3],
+            [6, 40, 2, 2, 5],
+            [6, 80, 3, 2, 3],
+            [6, 112, 3, 1, 5],
+            [6, 192, 4, 2, 5],
+            [6, 320, 1, 1, 3]
+        ]
+
+        out_channels = _round_filters(32, width_mult)
+        features = [ConvBNReLU(3, out_channels, 3, stride=2)]
+        in_channels = out_channels
+
+        for t, c, n, s, k in settings:
+            out_channels = _round_filters(c, width_mult)
+            repeats = _round_repeats(n, depth_mult)
+            for i in range(repeats):
+                stride = s if i == 0 else 1
+                features += [
+                    MBConvBlock(
+                        in_channels,
+                        out_channels,
+                        expand_ratio=t,
+                        stride=stride,
+                        kernel_size=k)
+                ]
+                in_channels = out_channels
+
+        last_channels = _round_filters(1280, width_mult)
+        features += [ConvBNReLU(in_channels, last_channels, 1)]
+
+        self.features = nn.Sequential(*features)
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(last_channels, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.mean([2, 3])
+        x = self.classifier(x)
+        return x
+
+
+def _efficientnet(arch, pretrained, progress, **kwargs):
+    params = {
+        'efficientnet-b0': (1.0, 1.0, 224, 0.2),
+        'efficientnet-b1': (1.0, 1.1, 240, 0.2),
+        'efficientnet-b2': (1.1, 1.2, 260, 0.3),
+        'efficientnet-b3': (1.2, 1.4, 300, 0.3),
+        'efficientnet-b4': (1.4, 1.8, 380, 0.4),
+        'efficientnet-b5': (1.6, 2.2, 456, 0.4),
+        'efficientnet-b6': (1.8, 2.6, 528, 0.5),
+        'efficientnet-b7': (2.0, 3.1, 600, 0.5),
+    }
+    width_mult, depth_mult, resolution, dropout_rate = params[arch]
+    model = EfficientNet(width_mult, depth_mult, dropout_rate, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(
+            model_urls[arch], progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def efficientnet_b0(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b0', pretrained, progress, **kwargs)
+
+
+def efficientnet_b1(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b1', pretrained, progress, **kwargs)
+
+
+def efficientnet_b2(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b2', pretrained, progress, **kwargs)
+
+
+def efficientnet_b3(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b3', pretrained, progress, **kwargs)
+
+
+def efficientnet_b4(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b4', pretrained, progress, **kwargs)
+
+
+def efficientnet_b5(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b5', pretrained, progress, **kwargs)
+
+
+def efficientnet_b6(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b6', pretrained, progress, **kwargs)
+
+
+def efficientnet_b7(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet-b7', pretrained, progress, **kwargs)
